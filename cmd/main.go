5567b32ab2b1a1c91563ffd6584774cef3c6bd55
nikita <nikitakerzhik@gmail.com>
2024-12-02 14:10:14 +1000

    realization starting server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"todoGRPC/internal/app"
+	"todoGRPC/internal/config"
+)
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting todo", slog.String("env", cfg.Env))
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath)
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCSrv.Stop()
+	log.Info("application stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	}
+	return log
 }
