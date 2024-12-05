da4f00820ff50bc7b082ffc057a601c7e31efbf4
nikita <nikitakerzhik@gmail.com>
2024-12-05 13:07:54 +1000

    implemented task service

--- internal/services/storage/postgres/postgres.go
+++ internal/services/storage/postgres/postgres.go
@@ -21,26 +21,26 @@ func New(storagePath string) (*Storage, error) {
 	return &Storage{db: db}, nil
 }
 
-func (s *Storage) SaveTask(ctx context.Context, name, description, deadline, token string) (*tasksv1.Task, error) {
+func (s *Storage) SaveTask(ctx context.Context, name, description, deadline string, userId int64) (int64, error) {
 	panic("implement me")
 }
 
-func (s *Storage) Tasks(ctx context.Context, page, countTaskOnPage int) ([]*tasksv1.Task, error) {
+func (s *Storage) Tasks(ctx context.Context, page, countTaskOnPage, userId int64) ([]*tasksv1.Task, error) {
 	panic("implement me")
 }
 
-func (s *Storage) TaskByID(ctx context.Context, id string) (*tasksv1.Task, error) {
+func (s *Storage) TaskByID(ctx context.Context, userId, taskId int64) (*tasksv1.Task, error) {
 	panic("implement me")
 }
 
-func (s *Storage) TaskByName(ctx context.Context, userId int64, name string) (*tasksv1.Task, error) {
+func (s *Storage) TaskByName(ctx context.Context, userId int64, name string) ([]*tasksv1.Task, error) {
 	panic("implement me")
 }
 
-func (s *Storage) UpdateTask(ctx context.Context, name, description, deadline, token string) (string, error) {
+func (s *Storage) UpdateTask(ctx context.Context, name, description, deadline string, userId int64) (string, error) {
 	panic("implement me")
 }
 
-func (s *Storage) DeleteTask(ctx context.Context, id string) (string, error) {
+func (s *Storage) DeleteTask(ctx context.Context, taskId, userId int64) (string, error) {
 	panic("implement me")
 }
--- internal/services/tasks/tasks.go
+++ internal/services/tasks/tasks.go
@@ -15,21 +15,21 @@ type Tasks struct {
 }
 
 type TaskSaver interface {
-	SaveTask(ctx context.Context, name, description, deadline, token string) (*tasksv1.Task, error)
+	SaveTask(ctx context.Context, name, description, deadline string, userId int64) (int64, error)
 }
 
 type TaskProvider interface {
-	Tasks(ctx context.Context, page, countTaskOnPage int) ([]*tasksv1.Task, error)
-	TaskByID(ctx context.Context, id string) (*tasksv1.Task, error)
-	TaskByName(ctx context.Context, userId int64, name string) (*tasksv1.Task, error)
+	Tasks(ctx context.Context, page, countTaskOnPage, userId int64) ([]*tasksv1.Task, error)
+	TaskByID(ctx context.Context, userId, taskId int64) (*tasksv1.Task, error)
+	TaskByName(ctx context.Context, userId int64, name string) ([]*tasksv1.Task, error)
 }
 
 type TaskUpdater interface {
-	UpdateTask(ctx context.Context, name, description, deadline, token string) (string, error)
+	UpdateTask(ctx context.Context, name, description, deadline string, userId int64) (string, error)
 }
 
 type TaskDeleter interface {
-	DeleteTask(ctx context.Context, id string) (string, error)
+	DeleteTask(ctx context.Context, taskId, userId int64) (string, error)
 }
 
 func New(log *slog.Logger,
@@ -46,26 +46,102 @@ func New(log *slog.Logger,
 	}
 }
 
-func (s *Tasks) Create(ctx context.Context, name, description, deadline string, userId int64) (id int64, err error) {
-	panic("implement me")
+func (s *Tasks) Create(ctx context.Context, name, description, deadline string, userId int64) (int64, error) {
+	const op = "tasks.Create"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.String("name", name),
+		slog.Int64("userId", userId))
+
+	log.Info("creating task")
+
+	id, err := s.taskSaver.SaveTask(ctx, name, description, deadline, userId)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+
+	log.Info("success create task")
+	return id, err
 }
 
 func (s *Tasks) Get(ctx context.Context, page, countTaskOnPage, userId int64) (tasks []*tasksv1.Task, err error) {
-	panic("implement me")
+	const op = "tasks.Get"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("page", page),
+		slog.Int64("userId", userId))
+
+	log.Info("getting tasks")
+	res, err := s.taskProvider.Tasks(ctx, page, countTaskOnPage, userId)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+
+	log.Info("success get tasks")
+	return res, nil
 }
 
 func (s *Tasks) GetByName(ctx context.Context, name string, userId int64) (tasks []*tasksv1.Task, err error) {
-	panic("implement me")
+	const op = "tasks.GetByName"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId))
+
+	log.Info("getting tasks")
+	res, err := s.taskProvider.TaskByName(ctx, userId, name)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+
+	log.Info("success get tasks")
+	return res, nil
 }
 
 func (s *Tasks) GetById(ctx context.Context, userId, taskId int64) (task *tasksv1.Task, err error) {
-	panic("implement me")
+	const op = "tasks.GetById"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId),
+		slog.Int64("taskId", taskId),
+	)
+
+	log.Info("getting task")
+	res, err := s.taskProvider.TaskByID(ctx, userId, taskId)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+	log.Info("success get task")
+	return res, nil
 }
 
 func (s *Tasks) Update(ctx context.Context, taskId int64, name, description, deadline string, userId int64) (message string, err error) {
-	panic("implement me")
+	const op = "tasks.Update"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("taskId", taskId),
+		slog.Int64("userId", userId))
+
+	log.Info("updating task")
+	res, err := s.taskUpdater.UpdateTask(ctx, name, description, deadline, userId)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+
+	log.Info("success update task")
+	return res, err
 }
 
 func (s *Tasks) Delete(ctx context.Context, taskId, userId int64) (message string, err error) {
-	panic("implement me")
+	const op = "tasks.Delete"
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("taskId", taskId),
+		slog.Int64("userId", userId))
+
+	log.Info("deleting task")
+	res, err := s.taskDeleter.DeleteTask(ctx, taskId, userId)
+	if err != nil {
+		// TODO: обработка ошибки
+	}
+	return res, nil
 }
