7212a7dd9be95c4ad649be90fbe2cc14e1447fc7
nikita <nikitakerzhik@gmail.com>
2024-12-12 14:35:06 +1000

    refactoring

--- internal/lib/mapper/mapper.go
+++ internal/lib/mapper/mapper.go
@@ -5,21 +5,6 @@ import (
 	"todoGRPC/internal/domain/models"
 )
 
-//func MapToTask(task *tasksv1.Task) (models.Task, error) {
-//	deadline, err := time.Parse("02.01.2006", task.Deadline)
-//	if err != nil {
-//		return models.Task{}, err
-//	}
-//	return models.Task{
-//		Name:         task.Name,
-//		Descriptions: task.Descriptions,
-//		TypeTask: models.TypeTask{
-//			TypeTask: task.TypeTask,
-//		},
-//		Deadline: deadline,
-//	}, nil
-//}
-
 func MapToTaskV1(task models.Task) *tasksv1.Task {
 	return &tasksv1.Task{
 		Id:           task.Id,
